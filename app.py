876b68b1c0e4ce2cce34a1c0758323400f44487c
RShnitser <romanshnitser@hotmail.com>
2024-07-31 18:41:42 -0400

    add flask

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
-
 client = OpenAI()
+app = Flask(__name__)
 
-response = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "This is a test."},
-    {"role": "user", "content": "Test"},
-  ]
-)
+@app.route("/")
+def home():
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "This is a test."},
+      {"role": "user", "content": "Test"},
+    ]
+  )
 
-print(response.choices[0])
+  return response.choices[0].message.content
