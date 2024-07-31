ebdcd827803241814db66a66fe7b1629ca999f89
RShnitser <romanshnitser@hotmail.com>
2024-07-31 19:15:01 -0400

    add index template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, render_template
 
 load_dotenv()
 client = OpenAI()
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  response = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "This is a test."},
-      {"role": "user", "content": "Test"},
-    ]
-  )
-
-  return response.choices[0].message.content
+  # response = client.chat.completions.create(
+  #   model="gpt-3.5-turbo",
+  #   messages=[
+  #     {"role": "system", "content": "This is a test."},
+  #     {"role": "user", "content": "Test"},
+  #   ]
+  # )
+  # text = response.choices[0].message.content
+  return render_template("index.html", answer="testing")
